8020745a23254df15240bd250ff97834c658171c
Arnaud ZANETTI <arnaud.zanetti@free.fr>
2026-09-22 13:54:19 +0200

    Use absolute path to run when started from crontab

--- script.bash
+++ script.bash
@@ -2,7 +2,7 @@
 while true
 do
 
-if uhd_find_devices --args serial=3155F91,type=b200,master_clock_rate=4e6 
+if /usr/local/bin/uhd_find_devices --args serial=3155F91,type=b200,master_clock_rate=4e6 
 then
 	echo "UHD device found, (re)starting the GPS contellation simulator"
 	~/gps-sdr-sim/gps-sdr-sim -p -s 1000000 -e ~/gps-sdr-sim/brdc1100.26n -l $(cat ~/gps-sdr-sim/eiffel.txt) -t 2026/04/20,01:00:00 -d 28800 -o - | /usr/local/lib/uhd/examples/tx_samples_stream --rate 1e6 --freq 1575420000 --gain 60 --args serial=3155F91,type=b200,master_clock_rate=4e6 
