e4120f05f0e1434f1a538a059e09c38c1f56ff8b
Arnaud ZANETTI <arnaud.zanetti@free.fr>
2026-09-22 13:39:27 +0200

    Make the run script smarter by first detecting the UHD device If the device is not present, wait for 15s before retrying. This avoid a race condition when only one UHD device is present and both LTE en GPS are trying to detect their respective ones.

--- script.bash
+++ script.bash
@@ -1,3 +1,16 @@
 #!/bin/bash
-(while true ; do ~/gps-sdr-sim/gps-sdr-sim -p -s 1000000 -e ~/gps-sdr-sim/brdc1100.26n -l $(cat ~/gps-sdr-sim/eiffel.txt) -t 2026/04/20,01:00:00 -d 28800 -o - | /usr/local/lib/uhd/examples/tx_samples_stream --rate 1e6 --freq 1575420000 --gain 60 --args serial=3155F91,type=b200,master_clock_rate=4e6 ; done)
+while true
+do
+
+if uhd_find_devices --args serial=3155F91,type=b200,master_clock_rate=4e6 
+then
+	echo "UHD device found, (re)starting the GPS contellation simulator"
+	~/gps-sdr-sim/gps-sdr-sim -p -s 1000000 -e ~/gps-sdr-sim/brdc1100.26n -l $(cat ~/gps-sdr-sim/eiffel.txt) -t 2026/04/20,01:00:00 -d 28800 -o - | /usr/local/lib/uhd/examples/tx_samples_stream --rate 1e6 --freq 1575420000 --gain 60 --args serial=3155F91,type=b200,master_clock_rate=4e6 
+else
+	echo "No UHD device found, waiting for 15 seconds before next try"
+	sleep 15
+fi
+
+done
+
 
